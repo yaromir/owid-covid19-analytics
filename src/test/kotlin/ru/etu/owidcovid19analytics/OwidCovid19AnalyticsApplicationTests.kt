3898a7798af775fb2278a8482a2d66586d7b75a8
Naveen <42085829+naveenpardeep@users.noreply.github.com>
2021-07-09 13:43:38 +0300

    Add files via upload

---
+++ src/test/kotlin/ru/etu/owidcovid19analytics/OwidCovid19AnalyticsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.etu.owidcovid19analytics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OwidCovid19AnalyticsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
