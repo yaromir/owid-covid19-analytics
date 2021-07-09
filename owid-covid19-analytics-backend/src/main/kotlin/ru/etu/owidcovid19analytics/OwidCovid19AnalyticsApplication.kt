335e0eb5e419fe1577ba6ef62145dbc4b46da597
Naveen <42085829+naveenpardeep@users.noreply.github.com>
2021-07-09 13:38:32 +0300

    [OWIDCOVID19-1] Created backend structure.

---
+++ owid-covid19-analytics-backend/src/main/kotlin/ru/etu/owidcovid19analytics/OwidCovid19AnalyticsApplication.kt
@@ -0,0 +1,11 @@
+package ru.etu.owidcovid19analytics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OwidCovid19AnalyticsApplication
+
+fun main(args: Array<String>) {
+    runApplication<OwidCovid19AnalyticsApplication>(*args)
+}
